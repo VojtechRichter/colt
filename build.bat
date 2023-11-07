2ab28b1ae78b49944fe1a1b900cf378b862db3ca
VojtechRichter <VojtechRichter>
2023-11-07 11:35:39 +0100

    Set binary output to specific name

--- build.bat
+++ build.bat
@@ -5,13 +5,14 @@ set CommonCompilerFlags=-Od -nologo -FC -WX -W4 -wd4201 -wd4100 -wd4505 -wd4189 
 set CommonLinkerFlags=
 
 set SourceDir=.\..\src\
-set Target=main
+set Target=colt
 
 del *.pdb > NUL 2> NUL
-REM Optimization switches: /O2 /Oi /fp:fast
 cd build
 
-cl %CommonCompilerFlags% %SourceDir%main.c %SourceDir%colt.c /link %CommonLinkerFlags%
+REM Optimization switches: /O2 /Oi /fp:fast
+REM TODO: make this less horrendous
+cl %CommonCompilerFlags% %SourceDir%main.c %SourceDir%colt.c /Fe:%Target% /link %CommonLinkerFlags%
 
 .\%Target%.exe
 cd ..\
